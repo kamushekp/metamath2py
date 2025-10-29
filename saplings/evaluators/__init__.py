27d813678a8c5f598b291ea32e3e2d9778cb11ad
Pavel <kamushekp@mail.ru>
2025-10-30 02:01:30 +0900

    add task 2

---
+++ saplings/evaluators/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from .proof_evaluator import ProofEvaluator
+
+__all__ = ["ProofEvaluator"]
+
---
+++ saplings/tools/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from .metamath_tools import SearchTheoremsTool, VerifyProofTool
+
+__all__ = [
+    "SearchTheoremsTool",
+    "VerifyProofTool",
+]
+
