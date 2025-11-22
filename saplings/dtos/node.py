39d1d4037f8a54cfc1814aa776ed01b752df1bf7
kamushek <kamushekp@gmail.com>
2025-11-23 02:24:46 +0700

    transfer commit

--- saplings/dtos/node.py
+++ saplings/dtos/node.py
@@ -1,24 +1,37 @@
 from __future__ import annotations
 
-from typing import List, Optional, Iterable
+from typing import List, Optional
 
 from saplings.dtos.tasks import TaskTransition, PatchSet, CreateNodeTask
 
 
 class Node(object):
-    def __init__(self, created_node_task: CreateNodeTask, parent_node: Optional["Node"] = None, created_from_patch_set: Optional[PatchSet] = None):
+    def __init__(
+        self,
+        created_node_task: CreateNodeTask,
+        parent_node: Optional["Node"] = None,
+        created_from_patch_set: Optional[PatchSet] = None,
+    ):
         self.id = id(self)
         self.created_node_task = created_node_task
-        self.parent_node =  parent_node
+        self.parent_node = parent_node
         self.created_from_patch_set = created_from_patch_set
         self.children: List["Node"] = []
 
     def get_trajectory(self) -> List[TaskTransition]:
         task_transitions: List[TaskTransition] = []
 
-        for node in self.traverse_to_root():
-            pass
+        nodes_to_root = self.traverse_to_root()
+        for parent, child in zip(nodes_to_root, nodes_to_root[1:]):
+            transition =  TaskTransition(
+                task_before=parent.created_node_task,
+                patch_set=child.created_from_patch_set,
+                task_after=child.created_node_task,
+            )
 
+            task_transitions.append(transition)
+
+        return task_transitions
 
     def traverse_to_root(self) -> List["Node"]:
         nodes = []
